22a20c306b0ed0f49be75714c1157f7e3eefc0a3
kdrag0n <danny@kdrag0n.dev>
2024-03-24 17:37:33 +0530

    sm6250-common: Use armv8-2a for 2nd arch variant Fixes error: Incorrect TARGET_2ND_ARCH_VARIANT, armv8-a. Use armv8-2a instead
    Change-Id: I061f35a6a4dd550c0bd2d528c3df7344752f735f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,7 +19,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
@@ -104,6 +104,7 @@ BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += kpti=off
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
